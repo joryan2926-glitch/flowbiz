30ddc721a1707e6e72669cf5442875bf7e3111d5
Jordan <joryan2926@gmail.com>
2026-05-18 20:30:46 +0200

    fix page cgu

---
+++ app/cgu/page.tsx
@@ -0,0 +1,9 @@
+export default function CguPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Conditions générales d’utilisation</h1>
+      <p>Page CGU en cours de finalisation.</p>
+    </main>
+  );
+}
+                       
